94f1b04bb5239cbb0315586b088281b558e347b6
jorditosa <jordisato88@gmail.com>
2023-04-11 15:27:20 +0200

    refactorizado some

--- PEC2_Ej3/e_some/core.js
+++ PEC2_Ej3/e_some/core.js
@@ -2,60 +2,46 @@
 // array are numbers greater than 10.
 
 function anyGreaterThan10 (input) {
-  input.some((element) => {
+  return input.some((element) => {
     if (element > 10) {
       return true;
     } else {
       return false;
     }
   });
-
-  return input;
 };
 
 // Check to see if any of the strings in
 // the array is longer than 10 characters.
 
 function longWord (input) {
-  input.some((element) => {
+  return input.some((element) => {
     if (element.length > 10) {
       return true;
     } else {
       return false;
     }
   });
-
-  return input;
 };
 
 // Check to see if any of the elements in
 // the matrix are true.
 
 function truePossibilities (input) {
-  input.some((element) => {
+  return input.some((element) => {
     if (element.some((element) => element === true)) {
       return true;
     } else {
       return false;
     }
   });
-
-  return input;
 };
 
 // Check to see if 'Lost' is in
 // the phrase (using some).
 
 function lostCarcosa (input) {
-  input.some((element) => {
-    if (element === "Lost") {
-      return true;
-    } else {
-      return false;
-    }
-  });
-  
-  return input;
+  return input.some((str) => str.toLowerCase().includes('lost'));
 };
 
 module.exports = {
