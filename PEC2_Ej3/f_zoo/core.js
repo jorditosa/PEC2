0227b211df6eb8d48743d0beaeac0d7b7daa81fc
jorditosa <jordisato88@gmail.com>
2023-04-11 08:00:57 +0200

    primeras funciones zoo hechas

--- PEC2_Ej3/f_zoo/core.js
+++ PEC2_Ej3/f_zoo/core.js
@@ -1,13 +1,36 @@
 function entryCalculator(entrants) {
-  // your code here
+  const { Adult = 0, Senior = 0, Child = 0 } = entrants;
+  return Adult * 49.99 + Senior * 24.99 + Child * 20.99;
 }
 
 function schedule(dayName) {
-  // your code here
+  const days = {
+    Monday: 'CLOSED',
+    Tuesday: 'Open from 8am until 6pm',
+    Wednesday: 'Open from 8am until 6pm',
+    Thursday: 'Open from 10am until 8pm',
+    Friday: 'Open from 10am until 8pm',
+    Saturday: 'Open from 8am until 10pm',
+    Sunday: 'Open from 8am until 8pm'
+  };
+  if (dayName) {
+    return { [dayName]: days[dayName] };
+  }
+  return days;
 }
 
 function animalCount(species) {
-  // your code here
+  const animals = data.animals;
+  const animalsCount = animals.reduce((acc, animal) => {
+    const { name, residents } = animal;
+    acc[name] = residents.length;
+    return acc;
+  }
+  , {});
+  if (species) {
+    return animalsCount[species];
+  }
+  return animalsCount;
 }
 
 function animalMap(options) {
@@ -15,11 +38,23 @@ function animalMap(options) {
 }
 
 function animalPopularity(rating) {
-  // your code here
+  const animals = data.animals;
+  const animalsByPopularity = animals.reduce((acc, animal) => {
+    const { name, residents } = animal;
+    const popularity = residents.length;
+    if (popularity >= rating) {
+      acc[name] = popularity;
+    }
+    return acc;
+  }
+  , {});
+  return animalsByPopularity;
 }
 
 function animalsByIds(ids) {
-  // your code here
+  const animals = data.animals;
+  const animalsById = animals.filter(animal => ids.includes(animal.id));
+  return animalsById;
 }
 
 function animalByName(animalName) {
